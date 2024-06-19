0f27c45de4a1ad770c662900ded18a5f32787983
John Gao <johnisaway285@gmail.com>
2024-06-19 10:09:07 -0600

    updated main(), tested finding Genre

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import requests,os
+import requests,os,re
 import openpyxl as excel
 from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+
 
 directory=os.getcwd()
 file_name="top_50.xlsx"
@@ -22,7 +18,10 @@ def main():
     soup=BeautifulSoup(raw_page.text,"html.parser")
     titles=soup.find_all("a",class_="hoverinfo_trigger")
     scores=soup.find_all("td",class_="score ac fs14")
-    link_to_synoposis=soup
+    genres=soup.find_all("div",class_="detail")
+    
+    print(genres)
+    '''
     for score in scores:
         if score.text == "\n\n":
             scores.pop(scores.index(score))
@@ -39,14 +38,12 @@ def main():
     book.save(file_path)
     print("Top 50 anime saved to top_50.xlsx")
         
-    
+   ''' 
 def create_empty():
     wb = excel.Workbook()
     wb.save(file_path)
 
-def get_synoposis(title:str):
-    driver=webdriver.Chrome()
-    driver.get("https://myanimelist.net/")
+
 
 if __name__=="__main__":
     main()
