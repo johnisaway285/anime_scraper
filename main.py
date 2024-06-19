110c236ae68685f7457d641ddc318f255acdddce
John Gao <johnisaway285@gmail.com>
2024-06-18 19:50:36 -0600

    Completed score collection

--- main.py
+++ main.py
@@ -16,20 +16,22 @@ def main():
     raw_page=requests.get("https://myanimelist.net/topanime.php",headers=headers)
     soup=BeautifulSoup(raw_page.text,"html.parser")
     titles=soup.find_all("a",class_="hoverinfo_trigger")
-    a={}
-    
+    scores=soup.find_all("td",class_="score ac fs14")
+    for score in scores:
+        if score.text == "\n\n":
+            scores.pop(scores.index(score))
     for title in titles:
         if title.text == "\n\n":
             titles.pop(titles.index(title))
-    for i in range(50):
-        a[i+1]=titles[i].text    
     sheet["A1"].value="Ranking"
     sheet["B1"].value="Anime Name"
-    for i in range(1,51):
-        sheet[f"A{i+1}"].value=i
-        sheet[f"B{i+1}"].value=a[i]
+    sheet["C1"].value="Score"
+    for i in range(0,50):
+        sheet[f"A{i+2}"].value=i+1
+        sheet[f"B{i+2}"].value=titles[i].text
+        sheet[f"C{i+2}"].value=scores[i].text
     book.save(file_path)
-
+    print("Top 50 anime saved to top_50.xlsx")
         
     
 def create_empty():
